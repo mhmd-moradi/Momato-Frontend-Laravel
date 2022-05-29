a3f63ad78546eaa713398603902dd62cbfd6f1ed
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-05-30 01:08:50 +0300

    Added global variables

---
+++ js/add-restaurants.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+
+    const image = document.getElementById("inputTag");
+    const name = document.getElementById("name");
+    const location = document.getElementById("location");
+    const category = document.getElementById("category");
+    const opening = document.getElementById("opening");
+    const opening_daytime = document.getElementById("opening-daytime");
+    const closing = document.getElementById("closing");
+    const closing_daytime = document.getElementById("closing-daytime");
+    const addBtn = document.getElementById("addBtn");
+ 
+}
