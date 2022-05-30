8e838e76562eeb63ee7b0abe84fa251adb12350c
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-05-31 02:36:00 +0300

    Push request

--- js/sign-up.js
+++ js/sign-up.js
@@ -7,17 +7,31 @@ window.onload = function () {
     const gender = document.querySelector('input[name="gender"]:checked').value;
     const username= document.getElementById("username").value;
     let button = document.getElementById("btn-sign-up");
-    button.addEventListener("click", signup);
+    button.addEventListener("click", function(event){
+    event.preventDefault();
+    
 
-    function signup() {
-        if (checkFields()){
-            insertSignup();
-        }      
-    }
-
-    function checkFields() {
-        if (fname != 0 && lname != "" && email != "" && username != "" && gender != "" && password != "" && c_password != "")
-            return true;
-        return false;
-    }
+    let data = new FormData();
+    data.append('username', username);
+    data.append('password', password);
+    data.append('email', email);
+    data.append('fname', fname);
+    data.append('lname', lname);
+    data.append('gender', gender);
+    axios({
+        method: 'post',
+        url: 'http://localhost/Momato/Momato-Backend/APIs/add_user.php',
+        data: data,
+    })
+    .then(function (response) {
+        console.log(response["data"]["id"]);
+        if(response.data.user_id == -1){
+            alert("Sign up failed!");
+        }else{
+            window.localStorage.setItem("id",response.data.user_id);
+            window.location.href = "display_restaurants.html";
+        }
+        }
+    );
+});
 }
