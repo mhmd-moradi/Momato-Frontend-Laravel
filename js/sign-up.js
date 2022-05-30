d30c9cbf98c3e783527eb62bf7f73493c3509c76
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-05-31 02:42:42 +0300

    Alerting on confirm password

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,15 +1,19 @@
 window.onload = function () {
+    let button = document.getElementById("btn-sign-up");
+    button.addEventListener("click", function(event){
+    event.preventDefault();
+
     const fname = document.getElementById("fname").value;
     const lname = document.getElementById("lname").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("pass").value;
     const c_password = document.getElementById("c_pass").value;
-    const gender = document.querySelector('input[name="gender"]:checked').value;
     const username= document.getElementById("username").value;
-    let button = document.getElementById("btn-sign-up");
-    button.addEventListener("click", function(event){
-    event.preventDefault();
+    const gender = document.querySelector('input[name="gender"]:checked').value;
     
+    if( password!== c_password){
+        alert("Passwords don't match");
+    }
 
     let data = new FormData();
     data.append('username', username);
