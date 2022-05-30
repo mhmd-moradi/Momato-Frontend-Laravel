ea2263a70f20a5658a539f65dea0b2b99a0c7990
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-05-31 02:12:16 +0300

    Adding function checkFields

--- js/sign-up.js
+++ js/sign-up.js
@@ -8,4 +8,16 @@ window.onload = function () {
     const username= document.getElementById("username").value;
     let button = document.getElementById("btn-sign-up");
     button.addEventListener("click", signup);
+
+    function signup() {
+        if (checkFields()){
+            insertSignup();
+        }      
     }
+
+    function checkFields() {
+        if (fname != 0 && lname != "" && email != "" && username != "" && gender != "" && password != "" && c_password != "")
+            return true;
+        return false;
+    }
+}
