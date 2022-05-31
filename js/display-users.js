466f55d261c7d8afc1f52dd67b158bdb72a4ff52
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-05-31 22:40:56 +0300

    Added getUsers function

--- js/display-users.js
+++ js/display-users.js
@@ -1,4 +1,24 @@
-if(window.localStorage.getItem("id")==-1){
-    console.log("checked")
-    window.location.href = "../index.html";
+window.onload = function () {
+
+    if(window.localStorage.getItem("id")==-1){
+        console.log("checked")
+        window.location.href = "../index.html";
+    }
+
+    const users_container = document.getElementById("users-container");
+
+    function getUsers(){
+        axios({
+            method: 'get',
+            url: 'http://localhost/Momato/Momato-Backend/APIs/get_users.php',
+        })
+        .then(function (response) {
+            for(let i=0; i < response["data"].length; i++){
+                createUser(response["data"][i].username, response["data"][i].gender, response["data"][i].email);
+            }
+        });
+    }
+
+    getUsers();
+
 }
