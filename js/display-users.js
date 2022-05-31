ee5b75ae822afe86b62a59d87b4f2c6815f6ebab
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-05-31 22:41:54 +0300

    Created the needed elements in createUser function

--- js/display-users.js
+++ js/display-users.js
@@ -19,6 +19,71 @@ window.onload = function () {
         });
     }
 
+    //create user dom
+    function createUser(username, gender, email){
+        let info = document.createElement('div');
+        info.setAttribute("class", "info");
+
+        let userdivmain = document.createElement('div');
+        userdivmain.setAttribute("class", "user-div");
+
+        let imagediv = document.createElement('div');
+        imagediv.setAttribute("class", "image");
+
+        let image = document.createElement('img');
+        image.setAttribute("src", "../assets/images/user4.jpg");
+        image.setAttribute("class", "user-profile");
+        image.setAttribute("id", "profile");
+
+        //username section
+        let userdiv = document.createElement('div');
+        userdiv.setAttribute("class", "userDiv");
+
+        let label_username = document.createElement('label');
+        label_username.innerHTML = "Username:"
+
+        let userd = document.createElement('div');
+        userd.setAttribute("class", "userd");
+
+        let usernametext = document.createElement('span');
+        usernametext.setAttribute("class", "username");
+        usernametext.setAttribute("id", "username");
+        usernametext.innerHTML = username;
+
+        //gender section
+        let genderdiv = document.createElement('div');
+        userdiv.setAttribute("class", "genderDiv");
+
+        let label_gender = document.createElement('label');
+        label_gender.setAttribute("class", "genderl");
+        label_gender.innerHTML = "Gender:"
+
+        let genderd = document.createElement('div');
+        genderd.setAttribute("class", "genderd");
+
+        let gendertext = document.createElement('span');
+        gendertext.setAttribute("class", "Gender");
+        gendertext.setAttribute("id", "gender");
+        gendertext.innerHTML = gender;
+
+        //email section
+        let emaildiv = document.createElement('div');
+        userdiv.setAttribute("class", "emailDiv");
+
+        let label_email = document.createElement('label');
+        label_email.setAttribute("class", "emaill");
+        label_email.innerHTML = "Email:"
+
+        let emaild = document.createElement('div');
+        genderd.setAttribute("class", "emaild");
+
+        let emailtext = document.createElement('span');
+        emailtext.setAttribute("class", "Email");
+        emailtext.setAttribute("id", "email");
+        emailtext.innerHTML = email;
+
+    }
+
     getUsers();
 
 }
