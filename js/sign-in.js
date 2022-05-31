d011c473a921c6a369a72f495ef1d6e4edde91be
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-05-31 18:20:51 +0300

    Handling errors in sign-in

--- js/sign-in.js
+++ js/sign-in.js
@@ -5,6 +5,12 @@ window.onload = function () {
 
     const username = document.getElementById("email").value;
     const password = document.getElementById("pass").value;
+    var error = document.getElementById("error");
+
+    if(username==="" || password===""){
+        error.innerText="Please fill all fields"
+    }
+    else{
 
     let data = new FormData();
     data.append('password', password);
@@ -17,8 +23,9 @@ window.onload = function () {
     .then(function (response) {
         console.log(response["data"]["id"]);
         if(response.data.user_id == -1){
-            alert("Sign in failed!");
+            error.innerText="Wrong username or password!"
         }else{
+            error.innerText=""
             window.localStorage.setItem("id",response.data.user_id);
             if(response.data.is_admin){
                 window.location.href = "html/admin-page.html";
@@ -29,6 +36,6 @@ window.onload = function () {
                 
         }
         }
-    );
+    );}
 });
 }
