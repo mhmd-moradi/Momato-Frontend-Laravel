444f35f8a83aa470d250f68dd24077404602e5e5
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-05-31 20:14:38 +0300

    Get api

--- js/user-profile.js
+++ js/user-profile.js
@@ -10,12 +10,24 @@ window.onload = function () {
         window.localStorage.setItem("id",-1)
     });
 
-    const fname = document.getElementById("fname").value;
-    const lname = document.getElementById("lname").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("pass").value;
-    const gender = document.querySelector('input[name="gender"]:checked').value;
+    const fname = document.getElementById("fname");
+    const lname = document.getElementById("lname");
+    const email = document.getElementById("email");
+    const password = document.getElementById("pass");
+    const gender = document.querySelector('input[name="gender"]:checked');
     var error = document.getElementById("error");
 
-    
+    let data = new FormData();
+    data.append('user_id', window.localStorage.getItem("id"));
+    axios({
+        method: 'post',
+        url: 'http://localhost/Momato/Momato-Backend/APIs/get_users.php',
+        data: data,
+    })
+    .then(function (response) {
+        console.log(response.data)
+        fname.
+        }
+    );
 }
+
